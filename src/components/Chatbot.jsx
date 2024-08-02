f762eef39936b9290f619a61614a27864e910836
atul-10-01 <145602768+atul-10-01@users.noreply.github.com>
2024-08-03 00:21:38 +0530

    fixing chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -240,10 +240,10 @@ const Chatbot = () => {
             ))}
             <div ref={chatEndRef} /> 
           </div>
-          <div className="p-3 flex items-center">
+          <div className="p-3 flex items-center w-[85%] sm:w-full">
             <input
               type="text"
-              className="flex-1 p-2 border bg-neutral-900 focus:ring-green-900 focus:border-green-900 border-gray-300 rounded-lg text-gray-50 font-extralight text-sm max-w-full"
+              className="flex-1 p-2 border bg-neutral-900 focus:ring-green-900 focus:border-green-900 border-gray-300 rounded-lg text-gray-50 font-extralight text-sm max-[300px]:w-[55vw]"
               value={inputValue}
               onChange={handleInputChange}
               onKeyPress={handleKeyPress}
