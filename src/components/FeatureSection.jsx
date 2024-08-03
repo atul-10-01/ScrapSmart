bbe049745fdb3ca9f21cd887280ffb7d82f20f87
atul-10-01 <145602768+atul-10-01@users.noreply.github.com>
2024-08-03 17:28:21 +0530

    feature-title change

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -8,7 +8,7 @@ const FeatureSection = () => {
           Features
         </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-5 lg:mt-10 tracking-wide">
-          Explore your{" "}
+          Explore{" "}
           <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
             ScrapSmart
           </span>
